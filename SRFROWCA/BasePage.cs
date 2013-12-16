2daf282c7211027ce171cba3908ed99500afa353
KashifNadeem <kashif.nadeem@hotmail.com>
2013-12-16 16:04:13 +0500

    Base class from page added gzip and CultureInitializtion

---
+++ SRFROWCA/BasePage.cs
@@ -0,0 +1,27 @@
+﻿
+using SRFROWCA.Common;
+using System;
+namespace SRFROWCA
+{
+    public class BasePage : System.Web.UI.Page
+    {
+        protected override void InitializeCulture()
+        {
+            string postBackControl = Request.Form["__EventTarget"];
+
+            if (!string.IsNullOrEmpty(postBackControl))
+            {
+                ROWCACommon.CultureSettings(postBackControl);
+            }
+
+            ROWCACommon.SetCulture();
+
+            base.InitializeCulture();
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            GZipContents.GZipOutput();
+        }
+    }
+}
