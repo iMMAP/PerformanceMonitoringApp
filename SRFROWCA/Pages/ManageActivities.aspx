05003128b033bda4ead000e458b2f670f79c1945
KashifNadeem <kashif.nadeem@hotmail.com>
2014-02-10 22:46:01 +0500

    Created new file to manage activities

---
+++ SRFROWCA/Pages/ManageActivities.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="ManageActivities.aspx.cs" Inherits="SRFROWCA.Pages.ManageActivities" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="containerDataEntryMain">
+        <div class="containerDataEntryProjects">
+            <div class="containerDataEntryProjectsInner">
+                test
+            </div>
+        </div>
+    </div>
+    <asp:Wizard ID="Wizard1" runat="server" OnNextButtonClick="wzrdReport_NextButtonClick"
+        OnPreviousButtonClick="wzrdReport_PreviousButtonClick" OnFinishButtonClick="wzrdReport_FinishButtonClick">
+        <WizardSteps>
+            <asp:WizardStep ID="WizardStep1" runat="server" Title="Step 1">
+                <div class="containerDataEntryGrid">
+                    <div class="tablegrid">
+                        <table>
+                            <tr>
+                                <td>
+                                    Projects:
+                                </td>
+                                <td>
+                                    <asp:DropDownList ID="ddlProjects" runat="server">
+                                    </asp:DropDownList>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <asp:CheckBoxList ID="cblLocation" runat="server" RepeatColumns="4">
+                                    </asp:CheckBoxList>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </asp:WizardStep>
+            <asp:WizardStep ID="WizardStep2" runat="server" Title="Step 2">
+                <div id="scrolledGridView" style="overflow-x: auto; width: 100%;">
+                    <asp:GridView ID="gvActivities" runat="server" AutoGenerateColumns="False" ShowHeaderWhenEmpty="True"
+                        HeaderStyle-BackColor="ButtonFace" DataKeyNames="PriorityActivityId" CssClass="imagetable"
+                        Width="100%" meta:resourcekey="gvActivitiesResource1">
+                        <HeaderStyle BackColor="Control"></HeaderStyle>
+                        <RowStyle CssClass="istrow" />
+                        <AlternatingRowStyle CssClass="altcolor" />
+                        <Columns>
+                            <asp:BoundField DataField="Objective" HeaderText="Objective"></asp:BoundField>
+                            <asp:BoundField DataField="HumanitarianPriority" HeaderText="HumanitarianPriority" />
+                            <asp:BoundField DataField="ActivityName" HeaderText="Activity" ItemStyle-Width="450px">
+                                <ItemStyle Width="450px"></ItemStyle>
+                            </asp:BoundField>
+                            <asp:TemplateField HeaderText="Bale Annual Target">
+                                <ItemTemplate>
+                                    <asp:TextBox ID="txtBale" runat="server" Text=""></asp:TextBox>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Banwa Annual Target">
+                                <ItemTemplate>
+                                    <asp:TextBox ID="txtBanwa" runat="server" Text=""></asp:TextBox>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </div>
+            </asp:WizardStep>
+        </WizardSteps>
+    </asp:Wizard>
+</asp:Content>
---
+++ SRFROWCA/Pages/ManageActivities.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using System.Data;
+using SRFROWCA.Common;
+
+namespace SRFROWCA.Pages
+{
+    public partial class ManageActivities : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            PopulateProjects();
+            PopulateLocations();
+
+        }
+
+        private void PopulateLocations()
+        {
+            cblLocation.DataValueField = "LocationId";
+            cblLocation.DataTextField = "LocationName";
+            DataTable dt = DBContext.GetData("GetAdmin2LocationsOfCountry", new object[] { 2 });
+            cblLocation.DataSource = dt;
+            cblLocation.DataBind();
+
+           
+        }
+
+        private void PopulateProjects()
+        {
+            ddlProjects.DataValueField = "ProjectId";
+            ddlProjects.DataTextField = "ProjectTitle";
+            DataTable dt = DBContext.GetData("GetAllProjectsTest");
+            ddlProjects.DataSource = dt;
+            ddlProjects.DataBind();
+
+            Session["testprojectdata"] = dt;
+            //
+        }
+
+        protected void wzrdReport_NextButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            if (e.NextStepIndex == 1)
+            {
+                PopulateLogFrame();
+            }
+            else if (e.NextStepIndex == 2)
+            {
+               
+            }
+        }
+
+        protected void wzrdReport_FinishButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            Save();
+            Response.Redirect("~/Pages/AddActivities.aspx");
+        }
+
+        private void PopulateLogFrame()
+        {
+            int projectId = Convert.ToInt32(ddlProjects.SelectedValue);
+
+            int clusterId = 0;
+            DataTable dt = Session["testprojectdata"] as DataTable;
+            foreach(DataRow dr in dt.Rows)
+            {
+                if(dr["ProjectId"].ToString() == projectId.ToString())
+                {
+                    clusterId = Convert.ToInt32( dr["ClusterId"]);
+                }
+            }
+
+            gvActivities.DataSource = DBContext.GetData("GetLogFrameOfClusterTest", new object[] { clusterId, 1 });
+            gvActivities.DataBind();
+        }
+
+        protected void wzrdReport_PreviousButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            if (e.NextStepIndex == 0)
+            {
+                PopulateProjects();
+                PopulateLocations();
+            }
+        }
+
+        private void Save()
+        {
+            foreach (GridViewRow row in gvActivities.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    int pId = Convert.ToInt32(gvActivities.DataKeys[row.RowIndex].Values["PriorityActivityId"].ToString());
+                    TextBox txtBale = row.FindControl("txtBale") as TextBox;
+                    int? tBale = null;
+                    if (txtBale != null)
+                    {
+                        tBale = string.IsNullOrEmpty(txtBale.Text.Trim()) ? (int?)null : Convert.ToInt32(txtBale.Text.Trim());
+                    }
+
+                    TextBox txtBanwa = row.FindControl("txtBanwa") as TextBox;
+                    int? tBanwa = null;
+                    if (txtBanwa != null)
+                    {
+                        tBanwa = string.IsNullOrEmpty(txtBanwa.Text.Trim()) ? (int?)null : Convert.ToInt32(txtBanwa.Text.Trim());
+                    }
+                    Guid userId = ROWCACommon.GetCurrentUserId();
+                    int projectId = Convert.ToInt32(ddlProjects.SelectedValue);
+
+                    if (tBale != null || tBanwa != null)
+                    {
+                        DBContext.Add("InsertUserActivityDataTest", new object[] { 2, pId, userId, projectId, DBNull.Value });
+                    }
+                }
+            }
+        }
+    }
+}
