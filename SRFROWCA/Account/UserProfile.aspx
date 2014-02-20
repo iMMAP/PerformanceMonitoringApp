e09b3e870b796a284a01572e115cb909c251f91f
KashifNadeem <kashif.nadeem@hotmail.com>
2014-02-20 12:46:49 +0500

    Added new file to allow user to update user profile

---
+++ SRFROWCA/Account/UserProfile.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="UserProfile.aspx.cs" Inherits="SRFROWCA.Account.UserProfile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div id="divMsg">
+    </div>
+    <div class="containerLogin">
+        <div class="graybarLogin">
+            My Profile
+        </div>
+        <div class="contentarea">
+            <div class="formdiv">
+                <div class="singalselect">
+                    <label>
+                        User Name:</label>
+                    <div>
+                        <asp:TextBox ID="txtUserName" runat="server" CssClass="ddlWidthAllData" Enabled="false"
+                            Style="background-color: ButtonFace;"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="singalselect">
+                    <label>
+                        Email:
+                    </label>
+                    <div>
+                        <asp:TextBox ID="txtEmail" runat="server" CssClass="ddlWidthAllData"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="rfv" runat="server" ControlToValidate="txtEmail"
+                            Text="Required" ErrorMessage="Email" ForeColor="Red"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="singalselect">
+                    <label>
+                        Phone:</label>
+                    <div>
+                        <asp:TextBox ID="txtPhone" MaxLength="50" CssClass="ddlWidthAllData" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div id="divCountry" runat="server" class="singalselect">
+                    <label>
+                        Country:</label>
+                    <div>
+                        <asp:DropDownList ID="ddlCountry" runat="server" Width="400px">
+                        </asp:DropDownList>
+                        <asp:RequiredFieldValidator ID="rfvCountry" runat="server" ErrorMessage="Country"
+                            Text="*" InitialValue="0" ControlToValidate="ddlCountry"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+            </div>
+            <div class="spacer" style="clear: both;">
+            </div>
+        </div>
+        <div>
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click"
+                CssClass="button_example" />
+            <table cellpadding="5" cellspacing="0" class="pstyle1">
+                <tr>
+                    <td class="signupheading2">
+                        <asp:UpdatePanel ID="updMessage" runat="server" UpdateMode="Conditional">
+                            <ContentTemplate>
+                                <asp:Label ID="lblMessage" runat="server" CssClass="error-message" Visible="false"
+                                    ViewStateMode="Disabled"></asp:Label>
+                            </ContentTemplate>
+                            <Triggers>
+                                <asp:AsyncPostBackTrigger ControlID="btnUpdate" EventName="Click" />
+                            </Triggers>
+                        </asp:UpdatePanel>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="spacer" style="clear: both;">
+        </div>
+        <div class="graybarcontainer">
+        </div>
+    </div>
+</asp:Content>
---
+++ SRFROWCA/Account/UserProfile.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data;
+using BusinessLogic;
+using SRFROWCA.Common;
+
+namespace SRFROWCA.Account
+{
+    public partial class UserProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+
+            this.Form.DefaultButton = this.btnUpdate.UniqueID;
+
+            PopulateCountries();
+                GetUserInformation();
+            
+        }
+
+        private void GetUserInformation()
+        {
+            Guid userId = ROWCACommon.GetCurrentUserId();
+            MembershipUser mu = Membership.GetUser(userId);
+            txtUserName.Text = mu.UserName;
+            txtEmail.Text = mu.Email;
+
+            DataTable dt = GetUserDetails(userId);
+            if (dt.Rows.Count > 0)
+            {
+                txtPhone.Text = dt.Rows[0]["PhoneNumber"].ToString();
+
+                string[] roles = Roles.GetRolesForUser(mu.UserName);
+
+                ddlCountry.SelectedValue = dt.Rows[0]["LocationId"].ToString();
+            }
+        }
+
+        private DataTable GetUserDetails(Guid userId)
+        {
+            return DBContext.GetData("GetUserDetails", new object[] { userId });
+        }
+
+        // Populate countries drop down.
+        private void PopulateCountries()
+        {
+            ddlCountry.DataValueField = "LocationId";
+            ddlCountry.DataTextField = "LocationName";
+
+            DataTable dt = GetCountries();
+            ddlCountry.DataSource = dt;
+            ddlCountry.DataBind();
+        }
+
+        private DataTable GetCountries()
+        {
+            int locationType = (int)ROWCACommon.LocationTypes.National;
+            DataTable dt = DBContext.GetData("GetLocationOnType", new object[] { locationType });
+
+            return dt.Rows.Count > 0 ? dt : new DataTable();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Guid userId = ROWCACommon.GetCurrentUserId();
+                MembershipUser mu = Membership.GetUser(userId);
+                mu.Email = txtEmail.Text;
+                Membership.UpdateUser(mu);
+                object[] userValues = GetUserValues(userId);
+                DBContext.Add("UpdateASPNetUserInfoWithoutOrg", userValues);
+                ShowMessage("You profile updated successfully!");
+            }
+            catch
+            {
+                ShowMessage("Some error occoured while updating your profile. Please contact to Admin of the site.",
+                    ROWCACommon.NotificationType.Error);
+            }
+            
+        }
+
+        private object[] GetUserValues(Guid userId)
+        {
+            int countryId = 0;
+            int.TryParse(ddlCountry.SelectedValue, out countryId);
+            string phone = txtPhone.Text.Trim().Length > 0 ? txtPhone.Text.Trim() : null;
+            return new object[] { userId, countryId, phone, DBNull.Value };
+        }
+
+        private void ShowMessage(string message, ROWCACommon.NotificationType notificationType = ROWCACommon.NotificationType.Success)
+        {
+            ROWCACommon.ShowMessage(this.Page, typeof(Page), UniqueID, message, notificationType, true, 500);
+        }
+
+        protected void Page_Error(object sender, EventArgs e)
+        {
+            // Get last error from the server
+            Exception exc = Server.GetLastError();
+            SRFROWCA.Common.ExceptionUtility.LogException(exc, "UpdateUser", this.User);
+        }
+    }
+}
