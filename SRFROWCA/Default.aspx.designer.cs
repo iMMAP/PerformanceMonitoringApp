962225c12bf53be90587bd43de47696509106b84
KashifNadeem <kashif.nadeem@hotmail.com>
2014-03-06 23:59:13 +0500

    Removed login controls from Default Added Default.aspx as home page in site.master

--- SRFROWCA/Default.aspx.designer.cs
+++ SRFROWCA/Default.aspx.designer.cs
@@ -11,23 +11,5 @@ namespace SRFROWCA {
     
     
     public partial class _Default {
-        
-        /// <summary>
-        /// RegisterHyperLink control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink RegisterHyperLink;
-        
-        /// <summary>
-        /// LoginUser control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Login LoginUser;
     }
 }
