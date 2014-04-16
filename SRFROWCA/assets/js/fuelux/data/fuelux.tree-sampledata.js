956ac23f2c67fca3b444485071a17abe8319e0a5
KashifNadeem <kashif.nadeem@hotmail.com>
2014-04-16 19:05:17 +0500

    css, fonts, javascript

--- SRFROWCA/assets/js/fuelux/data/fuelux.tree-sampledata.js
+++ SRFROWCA/assets/js/fuelux/data/fuelux.tree-sampledata.js
@@ -103,74 +103,74 @@ var treeDataSource = new DataSourceTree({data: tree_data});
 
 
 var tree_data_2 = {
-	'pictures' : {name: 'Pictures', type: 'folder', 'icon-class':'red'}	,
-	'music' : {name: 'Music', type: 'folder', 'icon-class':'orange'}	,
-	'video' : {name: 'Video', type: 'folder', 'icon-class':'blue'}	,
-	'documents' : {name: 'Documents', type: 'folder', 'icon-class':'green'}	,
+	'pictures' : {name: 'Pictures', type: 'folder', 'fa fa-class':'red'}	,
+	'music' : {name: 'Music', type: 'folder', 'fa fa-class':'orange'}	,
+	'video' : {name: 'Video', type: 'folder', 'fa fa-class':'blue'}	,
+	'documents' : {name: 'Documents', type: 'folder', 'fa fa-class':'green'}	,
 	'backup' : {name: 'Backup', type: 'folder'}	,
-	'readme' : {name: '<i class="icon-file-text grey"></i> ReadMe.txt', type: 'item'},
-	'manual' : {name: '<i class="icon-book blue"></i> Manual.html', type: 'item'}
+	'readme' : {name: '<i class="fa fa-file-text grey"></i> ReadMe.txt', type: 'item'},
+	'manual' : {name: '<i class="fa fa-book blue"></i> Manual.html', type: 'item'}
 }
 tree_data_2['music']['additionalParameters'] = {
 	'children' : [
-		{name: '<i class="icon-music blue"></i> song1.ogg', type: 'item'},
-		{name: '<i class="icon-music blue"></i> song2.ogg', type: 'item'},
-		{name: '<i class="icon-music blue"></i> song3.ogg', type: 'item'},
-		{name: '<i class="icon-music blue"></i> song4.ogg', type: 'item'},
-		{name: '<i class="icon-music blue"></i> song5.ogg', type: 'item'}
+		{name: '<i class="fa fa-music blue"></i> song1.ogg', type: 'item'},
+		{name: '<i class="fa fa-music blue"></i> song2.ogg', type: 'item'},
+		{name: '<i class="fa fa-music blue"></i> song3.ogg', type: 'item'},
+		{name: '<i class="fa fa-music blue"></i> song4.ogg', type: 'item'},
+		{name: '<i class="fa fa-music blue"></i> song5.ogg', type: 'item'}
 	]
 }
 tree_data_2['video']['additionalParameters'] = {
 	'children' : [
-		{name: '<i class="icon-film blue"></i> movie1.avi', type: 'item'},
-		{name: '<i class="icon-film blue"></i> movie2.avi', type: 'item'},
-		{name: '<i class="icon-film blue"></i> movie3.avi', type: 'item'},
-		{name: '<i class="icon-film blue"></i> movie4.avi', type: 'item'},
-		{name: '<i class="icon-film blue"></i> movie5.avi', type: 'item'}
+		{name: '<i class="fa fa-film blue"></i> movie1.avi', type: 'item'},
+		{name: '<i class="fa fa-film blue"></i> movie2.avi', type: 'item'},
+		{name: '<i class="fa fa-film blue"></i> movie3.avi', type: 'item'},
+		{name: '<i class="fa fa-film blue"></i> movie4.avi', type: 'item'},
+		{name: '<i class="fa fa-film blue"></i> movie5.avi', type: 'item'}
 	]
 }
 tree_data_2['pictures']['additionalParameters'] = {
 	'children' : {
-		'wallpapers' : {name: 'Wallpapers', type: 'folder', 'icon-class':'pink'},
-		'camera' : {name: 'Camera', type: 'folder', 'icon-class':'pink'}
+		'wallpapers' : {name: 'Wallpapers', type: 'folder', 'fa fa-class':'pink'},
+		'camera' : {name: 'Camera', type: 'folder', 'fa fa-class':'pink'}
 	}
 }
 tree_data_2['pictures']['additionalParameters']['children']['wallpapers']['additionalParameters'] = {
 	'children' : [
-		{name: '<i class="icon-picture green"></i> wallpaper1.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> wallpaper2.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> wallpaper3.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> wallpaper4.jpg', type: 'item'}
+		{name: '<i class="fa fa-picture green"></i> wallpaper1.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> wallpaper2.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> wallpaper3.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> wallpaper4.jpg', type: 'item'}
 	]
 }
 tree_data_2['pictures']['additionalParameters']['children']['camera']['additionalParameters'] = {
 	'children' : [
-		{name: '<i class="icon-picture green"></i> photo1.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> photo2.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> photo3.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> photo4.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> photo5.jpg', type: 'item'},
-		{name: '<i class="icon-picture green"></i> photo6.jpg', type: 'item'}
+		{name: '<i class="fa fa-picture green"></i> photo1.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> photo2.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> photo3.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> photo4.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> photo5.jpg', type: 'item'},
+		{name: '<i class="fa fa-picture green"></i> photo6.jpg', type: 'item'}
 	]
 }
 
 
 tree_data_2['documents']['additionalParameters'] = {
 	'children' : [
-		{name: '<i class="icon-file-text red"></i> document1.pdf', type: 'item'},
-		{name: '<i class="icon-file-text grey"></i> document2.doc', type: 'item'},
-		{name: '<i class="icon-file-text grey"></i> document3.doc', type: 'item'},
-		{name: '<i class="icon-file-text red"></i> document4.pdf', type: 'item'},
-		{name: '<i class="icon-file-text grey"></i> document5.doc', type: 'item'}
+		{name: '<i class="fa fa-file-text red"></i> document1.pdf', type: 'item'},
+		{name: '<i class="fa fa-file-text grey"></i> document2.doc', type: 'item'},
+		{name: '<i class="fa fa-file-text grey"></i> document3.doc', type: 'item'},
+		{name: '<i class="fa fa-file-text red"></i> document4.pdf', type: 'item'},
+		{name: '<i class="fa fa-file-text grey"></i> document5.doc', type: 'item'}
 	]
 }
 
 tree_data_2['backup']['additionalParameters'] = {
 	'children' : [
-		{name: '<i class="icon-archive brown"></i> backup1.zip', type: 'item'},
-		{name: '<i class="icon-archive brown"></i> backup2.zip', type: 'item'},
-		{name: '<i class="icon-archive brown"></i> backup3.zip', type: 'item'},
-		{name: '<i class="icon-archive brown"></i> backup4.zip', type: 'item'}
+		{name: '<i class="fa fa-archive brown"></i> backup1.zip', type: 'item'},
+		{name: '<i class="fa fa-archive brown"></i> backup2.zip', type: 'item'},
+		{name: '<i class="fa fa-archive brown"></i> backup3.zip', type: 'item'},
+		{name: '<i class="fa fa-archive brown"></i> backup4.zip', type: 'item'}
 	]
 }
 var treeDataSource2 = new DataSourceTree({data: tree_data_2});
---
+++ SRFROWCA/assets/orsjs/dashboard.js
@@ -0,0 +1,46 @@
+﻿function PieChart() {
+    $(function () {
+        $('#piechart-placeholder').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                height: 300
+            },
+            credits: {
+                enabled: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y}</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000'                        
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Clusters',
+                data: [
+                ['Protection', 5739189255],
+                ['Health', 4116353971],
+                ['Nutrition', 2693950766],
+                ['Food Security', 1136654946],
+                ['Earyly Recovery', 794556139],
+                ['WASH', 513598769],
+                ['Emg Cluster', 432233435]
+                ['Eduction', 162597949]
+            ]
+            }]
+        });
+    });
+}
