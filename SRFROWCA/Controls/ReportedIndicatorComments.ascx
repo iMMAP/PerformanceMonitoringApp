c9146b4a2a458b57b64fc07cd63561a27c8d1a65
KashifNadeem <kashif.nadeem@hotmail.com>
2014-04-27 17:35:59 +0500

    Indicator Comments control

---
+++ SRFROWCA/Controls/ReportedIndicatorComments.ascx
@@ -0,0 +1,47 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ReportedIndicatorComments.ascx.cs"
+    Inherits="SRFROWCA.Controls.ReportedIndicatorComments" %>
+<%@ Register Assembly="FredCK.FCKeditorV2" Namespace="FredCK.FCKeditorV2" TagPrefix="FCKeditorV2" %>
+<script type="text/javascript">
+    $(document).ready(function () {
+        // scrollables
+        $('.slim-scroll').each(function () {
+            var $this = $(this);
+            $this.slimScroll({
+                height: $this.data('height') || 100,
+                railVisible: true
+            });
+        });
+    });
+</script>
+<div class="widget-box">
+    <div class="widget-header widget-header-small header-color-blue2">
+        Old Comments
+    </div>
+    <div class="widget-body">
+        <div class="widget-main">
+            <div class="slim-scroll" data-height="250">
+                <asp:Repeater ID="rptIndComments" runat="server">
+                    <ItemTemplate>
+                        <div class="widget-box">
+                            <div class="widget-header widget-header-small">
+                                <span class="grey">
+                                    <%#Eval("UserName")%></span> <span class="widget-toolbar no-border">
+                                        <%#Eval("CreatedDate")%></span>
+                            </div>
+                            <div class="widget-body">
+                                <div class="widget-main">
+                                    <span>
+                                        <%#Eval("Comments")%></span>
+                                </div>
+                            </div>
+                        </div>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+    <FCKeditorV2:FCKeditor ID="fcComments" runat="server" ToolbarSet="Basic">
+    </FCKeditorV2:FCKeditor>
+</div>
---
+++ SRFROWCA/Controls/ReportedIndicatorComments.ascx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using SRFROWCA.Common;
+
+namespace SRFROWCA.Controls
+{
+    public partial class ReportedIndicatorComments : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void LoadComments()
+        {
+            using (ORSEntities db = new ORSEntities())
+            {
+                rptIndComments.DataSource = DBContext.GetData("GetIndicatorComments", new object[] { YearId, MonthId, ProjectId, EmgLocationId, ActivityDataId });
+                rptIndComments.DataBind();
+            }
+        }
+
+        //protected void btnSaveComments_Click(object sender, EventArgs e)
+        //{
+        //    string comments = fcComments.Value;
+        //    if (!string.IsNullOrEmpty(comments))
+        //    {
+        //        using (ORSEntities db = new ORSEntities())
+        //        {
+        //            DBContext.Add("InsertIndicatorComments", new object[] { YearId, MonthId, ProjectId, ActivityDataId, EmgLocationId, comments, RC.GetCurrentUserId, DBNull.Value });
+        //        }
+
+        //        fcComments.Value = "";
+        //    }
+        //}
+
+        public string GetComments()
+        {
+            string comments = fcComments.Value;
+            fcComments.Value = "";
+            return comments;
+        }
+
+        public int ActivityDataId
+        {
+            get
+            {
+                int id = 0;
+                if (ViewState["ActivityDataId"] != null)
+                {
+                    int.TryParse(ViewState["ActivityDataId"].ToString(), out id);
+                }
+
+                return id;
+            }
+            set
+            {
+                ViewState["ActivityDataId"] = value;
+            }
+        }
+        public int YearId
+        {
+            get
+            {
+                int id = 0;
+                if (ViewState["YearId"] != null)
+                {
+                    int.TryParse(ViewState["YearId"].ToString(), out id);
+                }
+
+                return id;
+            }
+            set
+            {
+                ViewState["YearId"] = value;
+            }
+        }
+        public int MonthId
+        {
+            get
+            {
+                int id = 0;
+                if (ViewState["MonthId"] != null)
+                {
+                    int.TryParse(ViewState["MonthId"].ToString(), out id);
+                }
+
+                return id;
+            }
+            set
+            {
+                ViewState["MonthId"] = value;
+            }
+        }
+        public int ProjectId
+        {
+            get
+            {
+                int id = 0;
+                if (ViewState["ProjectId"] != null)
+                {
+                    int.TryParse(ViewState["ProjectId"].ToString(), out id);
+                }
+
+                return id;
+            }
+            set
+            {
+                ViewState["ProjectId"] = value;
+            }
+        }
+        public int EmgLocationId
+        {
+            get
+            {
+                int id = 0;
+                if (ViewState["EmgLocationId"] != null)
+                {
+                    int.TryParse(ViewState["EmgLocationId"].ToString(), out id);
+                }
+
+                return id;
+            }
+            set
+            {
+                ViewState["EmgLocationId"] = value;
+            }
+        }
+
+    }
+}
