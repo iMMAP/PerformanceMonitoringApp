9c860a226f59603616ac3dca434d01f7fb8e6199
KashifNadeem <kashif.nadeem@hotmail.com>
2014-05-10 20:13:29 +0500

    on reportid and activitydataid

--- SRFROWCA/Controls/ReportedIndicatorComments.ascx.cs
+++ SRFROWCA/Controls/ReportedIndicatorComments.ascx.cs
@@ -17,28 +17,14 @@ namespace SRFROWCA.Controls
 
         }
 
-        public void LoadComments()
+        public void LoadComments(int reportId, int activityDataId)
         {
             using (ORSEntities db = new ORSEntities())
             {
-                rptIndComments.DataSource = DBContext.GetData("GetIndicatorComments", new object[] { YearId, MonthId, ProjectId, EmgLocationId, ActivityDataId });
+                rptIndComments.DataSource = DBContext.GetData("GetIndicatorComments", new object[] {reportId, activityDataId });
                 rptIndComments.DataBind();
             }
         }
-
-        //protected void btnSaveComments_Click(object sender, EventArgs e)
-        //{
-        //    string comments = fcComments.Value;
-        //    if (!string.IsNullOrEmpty(comments))
-        //    {
-        //        using (ORSEntities db = new ORSEntities())
-        //        {
-        //            DBContext.Add("InsertIndicatorComments", new object[] { YearId, MonthId, ProjectId, ActivityDataId, EmgLocationId, comments, RC.GetCurrentUserId, DBNull.Value });
-        //        }
-
-        //        fcComments.Value = "";
-        //    }
-        //}
 
         public string GetComments()
         {
@@ -47,91 +33,92 @@ namespace SRFROWCA.Controls
             return comments;
         }
 
-        public int ActivityDataId
-        {
-            get
-            {
-                int id = 0;
-                if (ViewState["ActivityDataId"] != null)
-                {
-                    int.TryParse(ViewState["ActivityDataId"].ToString(), out id);
-                }
+        //public int ActivityDataId
+        //{
+        //    get
+        //    {
+        //        int id = 0;
+        //        if (ViewState["ActivityDataId"] != null)
+        //        {
+        //            int.TryParse(ViewState["ActivityDataId"].ToString(), out id);
+        //        }
 
-                return id;
-            }
-            set
-            {
-                ViewState["ActivityDataId"] = value;
-            }
-        }
-        public int YearId
-        {
-            get
-            {
-                int id = 0;
-                if (ViewState["YearId"] != null)
-                {
-                    int.TryParse(ViewState["YearId"].ToString(), out id);
-                }
+        //        return id;
+        //    }
+        //    set
+        //    {
+        //        ViewState["ActivityDataId"] = value;
+        //    }
+        //}
 
-                return id;
-            }
-            set
-            {
-                ViewState["YearId"] = value;
-            }
-        }
-        public int MonthId
-        {
-            get
-            {
-                int id = 0;
-                if (ViewState["MonthId"] != null)
-                {
-                    int.TryParse(ViewState["MonthId"].ToString(), out id);
-                }
+        //public int YearId
+        //{
+        //    get
+        //    {
+        //        int id = 0;
+        //        if (ViewState["YearId"] != null)
+        //        {
+        //            int.TryParse(ViewState["YearId"].ToString(), out id);
+        //        }
 
-                return id;
-            }
-            set
-            {
-                ViewState["MonthId"] = value;
-            }
-        }
-        public int ProjectId
-        {
-            get
-            {
-                int id = 0;
-                if (ViewState["ProjectId"] != null)
-                {
-                    int.TryParse(ViewState["ProjectId"].ToString(), out id);
-                }
+        //        return id;
+        //    }
+        //    set
+        //    {
+        //        ViewState["YearId"] = value;
+        //    }
+        //}
+        //public int MonthId
+        //{
+        //    get
+        //    {
+        //        int id = 0;
+        //        if (ViewState["MonthId"] != null)
+        //        {
+        //            int.TryParse(ViewState["MonthId"].ToString(), out id);
+        //        }
 
-                return id;
-            }
-            set
-            {
-                ViewState["ProjectId"] = value;
-            }
-        }
-        public int EmgLocationId
-        {
-            get
-            {
-                int id = 0;
-                if (ViewState["EmgLocationId"] != null)
-                {
-                    int.TryParse(ViewState["EmgLocationId"].ToString(), out id);
-                }
+        //        return id;
+        //    }
+        //    set
+        //    {
+        //        ViewState["MonthId"] = value;
+        //    }
+        //}
+        //public int ProjectId
+        //{
+        //    get
+        //    {
+        //        int id = 0;
+        //        if (ViewState["ProjectId"] != null)
+        //        {
+        //            int.TryParse(ViewState["ProjectId"].ToString(), out id);
+        //        }
 
-                return id;
-            }
-            set
-            {
-                ViewState["EmgLocationId"] = value;
-            }
-        }
+        //        return id;
+        //    }
+        //    set
+        //    {
+        //        ViewState["ProjectId"] = value;
+        //    }
+        //}
+        //public int EmgLocationId
+        //{
+        //    get
+        //    {
+        //        int id = 0;
+        //        if (ViewState["EmgLocationId"] != null)
+        //        {
+        //            int.TryParse(ViewState["EmgLocationId"].ToString(), out id);
+        //        }
+
+        //        return id;
+        //    }
+        //    set
+        //    {
+        //        ViewState["EmgLocationId"] = value;
+        //    }
+        //}
 
     }
 }
