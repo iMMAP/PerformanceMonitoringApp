d587889201249267493f68da54d9d3d53356fa9f
KashifNadeem <kashif.nadeem@hotmail.com>
2014-06-11 18:28:29 +0500

    Removed Page_Init

--- SRFROWCA/Account/ChangePasswordSuccess.aspx.cs
+++ SRFROWCA/Account/ChangePasswordSuccess.aspx.cs
@@ -5,11 +5,6 @@ namespace SRFROWCA.Account
 {
     public partial class ChangePasswordSuccess : BasePage
     {
-        protected void Page_PreInit(object sender, EventArgs e)
-        {
-            GZipContents.GZipOutput();
-        }
-
         protected void Page_Load(object sender, EventArgs e)
         {
 
