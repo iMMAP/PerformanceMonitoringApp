15f0e3fff1c40dd32e3058ba5597d417eff398a0
KashifNadeem <kashif.nadeem@hotmail.com>
2014-06-11 18:29:41 +0500

    Removed
    SelectedValues
    SelectedItems

--- SRFROWCA/ReportsCharts/ReportsCommon.cs
+++ SRFROWCA/ReportsCharts/ReportsCommon.cs
@@ -72,43 +72,7 @@ namespace SRFROWCA.Reports
 
             return series;
         }
-
-        // Get multiple selected values from drop down checkbox.
-        public static string GetSelectedValues(object sender)
-        {
-            string ids = GetSelectedItems(sender);
-            ids = !string.IsNullOrEmpty(ids) ? ids : null;
-            return ids;
-        }
-
-        public static int? GetSelectedValue(DropDownList ddl)
-        {
-            int val = 0;
-            int.TryParse(ddl.SelectedValue, out val);
-            return val > 0 ? val : (int?)null;
-        }
-
-        private static string GetSelectedItems(object sender)
-        {
-            string itemIds = "";
-            foreach (ListItem item in (sender as ListControl).Items)
-            {
-                if (item.Selected)
-                {
-                    if (itemIds != "")
-                    {
-                        itemIds += "," + item.Value;
-                    }
-                    else
-                    {
-                        itemIds += item.Value;
-                    }
-                }
-            }
-
-            return itemIds;
-        }
-
+        
         public static void UpdateNullColumns(DataTable dt)
         {
             foreach (DataRow row in dt.Rows)
