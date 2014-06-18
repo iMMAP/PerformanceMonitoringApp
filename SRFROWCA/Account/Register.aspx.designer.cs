d4bfdb042afbc68d1aade6fa4718f93fa81c7598
KashifNadeem <kashif.nadeem@hotmail.com>
2014-06-18 11:40:13 +0500

    Removed Reset button

--- SRFROWCA/Account/Register.aspx.designer.cs
+++ SRFROWCA/Account/Register.aspx.designer.cs
@@ -211,15 +211,6 @@ namespace SRFROWCA.Account {
         protected global::System.Web.UI.WebControls.CustomValidator CustomValidator1;
         
         /// <summary>
-        /// btnReset control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnReset;
-        
-        /// <summary>
         /// btnRegister control.
         /// </summary>
         /// <remarks>
