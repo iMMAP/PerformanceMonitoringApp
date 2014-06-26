6b29dcf202d59ffb5507320e1c789c0f3366f165
waseempk <waseem.asghar.pk@gmail.com>
2014-06-26 23:21:19 +0500

    Emergency related Issue fixing

--- SRFROWCA/Admin/EmergencyLocations.aspx.designer.cs
+++ SRFROWCA/Admin/EmergencyLocations.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace SRFROWCA.Admin {
         protected global::System.Web.UI.WebControls.CheckBoxList cblLocations;
         
         /// <summary>
+        /// cvmodulelist control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator cvmodulelist;
+        
+        /// <summary>
         /// btnSave control.
         /// </summary>
         /// <remarks>
