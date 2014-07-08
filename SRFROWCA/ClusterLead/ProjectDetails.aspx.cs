ae067e5e9167e371ac2d2ea8f948d903e0af0aec
abdullahpk85 <abdullah.pk85@hotmail.com>
2014-07-08 20:31:52 +0500

    Abdullah - PDF changes, replacing ProjectReports

--- SRFROWCA/ClusterLead/ProjectDetails.aspx.cs
+++ SRFROWCA/ClusterLead/ProjectDetails.aspx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using BusinessLogic;
+using SRFROWCA.Common;
 
 namespace SRFROWCA.ClusterLead
 {
@@ -19,6 +20,7 @@ namespace SRFROWCA.ClusterLead
             if (Request.QueryString["pid"] != null)
             {
                 GetProjectDetails();
+                LoadReports();
             }
         }
 
@@ -32,20 +34,44 @@ namespace SRFROWCA.ClusterLead
             fvProjects.DataBind();
         }
 
+        private void LoadReports()
+        {
+            grdReports.DataSource = GetReports();
+            grdReports.DataBind();
+        }
+
+        private DataTable GetReports()
+        {
+            int projectID = 0;
+
+            if (Request.QueryString["pid"] != null)
+                int.TryParse(Request.QueryString["pid"].ToString(), out projectID);
+
+            string startDate = /*!string.IsNullOrEmpty(txtFromDate.Text) ? txtFromDate.Text :*/ null;
+            string endDate = /*!string.IsNullOrEmpty(txtToDate.Text) ? txtToDate.Text :*/ null;
+
+            DataTable dtGrid = DBContext.GetData("uspGetReports", new object[] { projectID, startDate, endDate, null, null });
+
+            string[] selectedColumns = new[] { "ReportID", "ReportName", "ProjectCode", "IsApproved", "Country", "ProjectID", "CreatedDate" };
+            DataTable dtFiltered = new DataView(dtGrid).ToTable(true, selectedColumns);
+
+            return dtFiltered;
+        }
+
         protected void fvProjects_PageIndexChanging(object sender, FormViewPageEventArgs e)
         {
 
         }
 
-        protected void btnViewReport_Click(object sender, EventArgs e)
-        {
-            string projectID = "0";
+        //protected void btnViewReport_Click(object sender, EventArgs e)
+        //{
+        //    string projectID = "0";
 
-            if (!string.IsNullOrEmpty(Request.QueryString["pid"]))
-                projectID = Convert.ToString(Request.QueryString["pid"]);
+        //    if (!string.IsNullOrEmpty(Request.QueryString["pid"]))
+        //        projectID = Convert.ToString(Request.QueryString["pid"]);
 
-            Response.Redirect("~/ClusterLead/ProjectReports.aspx?pid=" + projectID);
-        }
+        //    Response.Redirect("~/ClusterLead/ProjectReports.aspx?pid=" + projectID);
+        //}
 
         protected void btExportPDF_Click(object sender, EventArgs e)
         {
@@ -54,7 +80,7 @@ namespace SRFROWCA.ClusterLead
             if (Request.QueryString["pid"] != null)
                 int.TryParse(Request.QueryString["pid"].ToString(), out projectID);
 
-            DataTable dtResults = DBContext.GetData("uspGetReports", new object[] { Convert.ToString(projectID), null, null , null});
+            DataTable dtResults = DBContext.GetData("uspGetReports", new object[] { Convert.ToString(projectID), null, null , null, null});
 
             if (dtResults.Rows.Count > 0)
             {
@@ -62,6 +88,77 @@ namespace SRFROWCA.ClusterLead
                 Response.AddHeader("Content-Disposition", string.Format("attachment;filename=Project-{0}-{1}.pdf", SRFROWCA.Common.UserInfo.CountryName, DateTime.Now.ToString("yyyyMMddHHmmss")));
                 Response.BinaryWrite(SRFROWCA.Common.WriteDataEntryPDF.GeneratePDF(dtResults, projectID, null).ToArray());
             }
+        }
+
+        protected void grdReports_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "PrintReport")
+            {
+                //int projectID = 65761;
+                int projectID = 0;
+
+                if (Request.QueryString["pid"] != null)
+                    int.TryParse(Request.QueryString["pid"].ToString(), out projectID);
+
+                DataTable dtResults = DBContext.GetData("uspGetReports", new object[] { Convert.ToString(projectID), null, null, Convert.ToInt32(e.CommandArgument), null });
+
+                if (dtResults.Rows.Count > 0)
+                {
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("Content-Disposition", string.Format("attachment;filename=Project-{0}-{1}.pdf", UserInfo.CountryName, DateTime.Now.ToString("yyyyMMddHHmmss")));
+                    Response.BinaryWrite(WriteDataEntryPDF.GeneratePDF(dtResults, projectID, Convert.ToInt32(e.CommandArgument)).ToArray());
+                }
+            }
+        }
+
+        protected void grdReports_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataTable dt = GetReports();
+            if (dt != null)
+            {
+                //Sort the data.
+                dt.DefaultView.Sort = e.SortExpression + " " + GetSortDirection(e.SortExpression);
+                grdReports.DataSource = dt;
+                grdReports.DataBind();
+            }
+        }
+
+        private string GetSortDirection(string column)
+        {
+            string sortDirection = "ASC";
+
+            string sortExpression = ViewState["SortExpression"] as string;
+
+            if (sortExpression != null)
+            {
+                if (sortExpression == column)
+                {
+                    string lastDirection = ViewState["SortDirection"] as string;
+                    if ((lastDirection != null) && (lastDirection == "ASC"))
+                    {
+                        sortDirection = "DESC";
+                    }
+                }
+            }
+
+            // Save new values in ViewState.
+            ViewState["SortDirection"] = sortDirection;
+            ViewState["SortExpression"] = column;
+
+            return sortDirection;
+        }
+
+        protected void grdReports_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdReports.PageIndex = e.NewPageIndex;
+            grdReports.SelectedIndex = -1;
+
+            LoadReports();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadReports();
         }
     }
 }
