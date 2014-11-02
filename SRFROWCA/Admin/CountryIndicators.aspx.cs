23716c9ba353b346c70fe754593258260f7d3677
abdullahpk85 <abdullah.pk85@hotmail.com>
2014-11-02 23:15:16 +0500

    Abdullah - CountryIndicators changes

---
+++ SRFROWCA/Admin/CountryIndicators.aspx.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLogic;
+using SRFROWCA.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SRFROWCA.Admin
+{
+    public partial class CountryIndicators : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadClusterLeads();
+                LoadCombos();
+            }
+        }
+
+        private void LoadCombos()
+        {
+            UI.FillCountry(ddlCountry);
+            UI.FillClusters(ddlCluster, RC.SelectedSiteLanguageId);
+        }
+
+        private void LoadClusterLeads()
+        {
+            string objective = null;
+            string indicator = null;
+            int? countryId = null;
+            int? clusterId = null;
+
+            if (!string.IsNullOrEmpty(txtObjectiveName.Text.Trim()))
+                objective = txtObjectiveName.Text;
+
+            if (!string.IsNullOrEmpty(txtIndicatorName.Text.Trim()))
+                indicator = txtIndicatorName.Text;
+
+            if (Convert.ToInt32(ddlCountry.SelectedValue) > -1)
+                countryId = Convert.ToInt32(ddlCountry.SelectedValue);
+
+            if (Convert.ToInt32(ddlCluster.SelectedValue) > -1)
+                clusterId = Convert.ToInt32(ddlCluster.SelectedValue);
+
+            gvClusterIndicators.DataSource = GetClusterIndicatros(clusterId, countryId, objective, indicator);
+            gvClusterIndicators.DataBind();
+        }
+
+        private DataTable GetClusterIndicatros(int? clusterId, int? countryId, string objective, string indicator)
+        {
+            return DBContext.GetData("uspGetCluserIndicators", new object[] { clusterId, countryId, objective, indicator, RC.SelectedSiteLanguageId });
+        }
+    }
+}
--- SRFROWCA/Site.Master.designer.cs
+++ SRFROWCA/Site.Master.designer.cs
@@ -1138,6 +1138,51 @@ namespace SRFROWCA {
         protected global::System.Web.UI.WebControls.Localize Localize6;
         
         /// <summary>
+        /// liClusterFrameworks control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liClusterFrameworks;
+        
+        /// <summary>
+        /// A6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A6;
+        
+        /// <summary>
+        /// localClusterFrameworksList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize localClusterFrameworksList;
+        
+        /// <summary>
+        /// liCountryIndicators control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liCountryIndicators;
+        
+        /// <summary>
+        /// Localize8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize Localize8;
+        
+        /// <summary>
         /// liActivities control.
         /// </summary>
         /// <remarks>
