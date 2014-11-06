e207ea8c0c64e55058c51d665e60fbf093f5dcb5
KashifNadeem <kashif.nadeem@hotmail.com>
2014-11-06 20:38:37 +0500

    !

--- SRFROWCA/ClusterLead/AddCountryIndicator.aspx.cs
+++ SRFROWCA/ClusterLead/AddCountryIndicator.aspx.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using BusinessLogic;
 using SRFROWCA.Common;
 using SRFROWCA.Controls;
@@ -273,4 +274,281 @@ namespace SRFROWCA.ClusterLead
             GetMaxCount("Key-" + countryId + clusterId);
         }
     }
+=======
+﻿using BusinessLogic;
+using SRFROWCA.Common;
+using SRFROWCA.Controls;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml; 
+
+namespace SRFROWCA.ClusterLead
+{
+    public partial class AddCountryIndicator : BasePage
+    {
+        public int maxCount = 0;
+        public bool applyFilter = false;
+        public string countryId = string.Empty;
+        public string clusterId = string.Empty;
+        public string displayNone = string.Empty; 
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            if (RC.IsClusterLead(this.User))
+            {
+                displayNone = "display:none";
+                countryId = Convert.ToString(UserInfo.EmergencyCountry);
+                clusterId = Convert.ToString(UserInfo.EmergencyCluster);
+
+                applyFilter = true;
+                SetMaxCount();
+
+                if(maxCount == 1)
+                    IndControlId = maxCount;
+            }
+            else
+                maxCount = 1;
+
+            //if (applyFilter)
+            {
+                string control = Utils.GetPostBackControlId(this);
+
+                //if (control == "ddlCountry" || control == "ddlCluster")
+                //    IndControlId = maxCount;
+
+                if ((applyFilter && IndControlId <= maxCount && control == "btnAddIndicatorControl")
+                    || control == "btnAddIndicatorControl")
+                    IndControlId += 1;
+                else
+                    btnAddIndicatorControl.Visible = false;
+
+                if (control == "btnRemoveIndicatorControl")
+                    IndControlId -= 1;
+
+                if (IndControlId <= 1)
+                    btnRemoveIndicatorControl.Visible = false;
+                else
+                    btnRemoveIndicatorControl.Visible = true;
+
+                if (applyFilter)
+                {
+                    if (IndControlId < maxCount)
+                        btnAddIndicatorControl.Visible = true;
+                    else
+                        btnAddIndicatorControl.Visible = false;
+                }
+                else
+                    btnAddIndicatorControl.Visible = true;
+
+                for (int i = 0; i < IndControlId; i++)
+                    AddIndicatorControl(i);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                UserInfo.UserProfileInfo();
+
+                LoadCombos();
+                ShowHideControls();
+                //PopulateObjective();
+
+                if (applyFilter)
+                    SetMaxCount();
+
+                if (maxCount > 0 && IndControlId == 0) 
+                {
+                    AddIndicatorControl(0);
+                    IndControlId = 1;
+                }
+            }
+        }
+
+        private void ShowHideControls()
+        {
+            if (RC.IsCountryAdmin(this.User))
+            {
+                lblCountry.Visible =
+                    ddlCountry.Visible = false;
+
+                ddlCountry.SelectedValue = Convert.ToString(UserInfo.EmergencyCountry);
+            }
+            else if (RC.IsClusterLead(this.User))
+            {
+                lblCountry.Visible =
+                    ddlCountry.Visible =
+                        ddlCluster.Visible =
+                            lblCluster.Visible = false;
+
+                ddlCountry.SelectedValue = Convert.ToString(UserInfo.EmergencyCountry);
+                ddlCluster.SelectedValue = Convert.ToString(UserInfo.EmergencyCluster);
+            }
+        }
+
+        internal override void BindGridData()
+        {
+            //PopulateObjective();
+            LoadCombos();
+        }
+
+        private void PopulateObjective()
+        {
+            LoadCombos();
+
+            //ListItem item = new ListItem("--- Select Objective ---", "0");
+            //ddlObjective.Items.Insert(0, item);
+        }
+
+        private void LoadCombos()
+        {
+            /*ddlObjective.DataValueField = "ObjectiveID";
+            ddlObjective.DataTextField = "Objective";
+
+            ddlObjective.DataSource = DBContext.GetData("GetObjectives", new object[] { RC.SelectedSiteLanguageId, null });
+            ddlObjective.DataBind();*/
+
+            UI.FillCountry(ddlCountry);
+            UI.FillClusters(ddlCluster, RC.SelectedSiteLanguageId);
+        }
+
+        public int IndControlId
+        {
+            get
+            {
+                int indControlId = 0;
+                if (ViewState["CountryIndicatorControlId"] != null)
+                {
+                    int.TryParse(ViewState["CountryIndicatorControlId"].ToString(), out indControlId);
+                }
+
+                return indControlId;
+            }
+            set
+            {
+                ViewState["CountryIndicatorControlId"] = value.ToString();
+            }
+        }
+
+        private void AddIndicatorControl(int i)
+        {
+            NewCountryIndicatorsControl newIndSet = (NewCountryIndicatorsControl)LoadControl("~/controls/NewCountryIndicatorsControl.ascx");
+            newIndSet.ControlNumber = i + 1;
+            newIndSet.ID = "countryIndicatorControlId" + i.ToString();
+            pnlIndicaotrs.Controls.Add(newIndSet);
+        }
+
+        protected void btnAddIndicatorControl_ServerClick(object sender, EventArgs e)
+        { }
+
+        protected void btnSave_ServerClick(object sender, EventArgs e)
+        {
+            SaveData();
+            Response.Redirect("~/ClusterLead/CountryIndicators.aspx");
+        }
+
+        private void SaveData()
+        {
+            //int objectiveID = RC.GetSelectedIntVal(ddlObjective);
+
+            //if (objectiveID > 0)
+            {
+                foreach (Control ctl in pnlIndicaotrs.Controls)
+                {
+                    if (ctl != null
+                        && ctl.ID != null
+                        && ctl.ID.Contains("countryIndicatorControlId"))
+                    {
+                        NewCountryIndicatorsControl indControl = ctl as NewCountryIndicatorsControl;
+
+                        if (indControl != null)
+                        {
+                            TextBox txtEng = (TextBox)indControl.FindControl("txtInd1Eng");
+                            TextBox txtFr = (TextBox)indControl.FindControl("txtInd1Fr");
+
+                            TextBox txtTarget = (TextBox)indControl.FindControl("txtTarget");
+                            DropDownList ddlUnits = (DropDownList)indControl.FindControl("ddlUnits");
+
+                            //indControl.SaveIndicators(objectiveID, Convert.ToInt32(ddlCountry.SelectedValue), Convert.ToInt32(ddlCluster.SelectedValue));
+                            indControl.SaveIndicators(Convert.ToInt32(ddlCountry.SelectedValue), Convert.ToInt32(ddlCluster.SelectedValue));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GetMaxCount(string configKey)
+        {
+            string PATH = HttpRuntime.AppDomainAppPath;
+            PATH = PATH.Substring(0, PATH.LastIndexOf(@"\") + 1) + @"Configurations\ChangeEndSettings.xml";
+
+            if (File.Exists(PATH))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(PATH);
+
+                XmlElement elem_settings = doc.GetElementById("ChangeEndSettings");
+                XmlNode settingsNode = doc.DocumentElement;
+
+                foreach (XmlNode node in settingsNode.ChildNodes)
+                {
+                    if (node.Name.Equals(configKey))
+                    {
+                        if (node.Attributes["ClusterCount"] != null)
+                            maxCount = Convert.ToInt32(node.Attributes["ClusterCount"].Value);
+                    }
+                }
+            }
+
+            if (maxCount > 0)
+            {
+                //string countryId = null;
+                //string clusterId = null;
+
+                if (Convert.ToInt32(ddlCountry.SelectedValue) > -1)
+                    countryId = ddlCountry.SelectedValue;
+
+                if (Convert.ToInt32(ddlCluster.SelectedValue) > -1)
+                    clusterId = ddlCluster.SelectedValue;
+
+                DataTable dtCount = DBContext.GetData("uspGetIndicatorCount", new object[] { countryId, clusterId });
+
+                if (dtCount.Rows.Count > 0)
+                    maxCount = maxCount - Convert.ToInt32(dtCount.Rows[0]["IndicatorCount"]);
+            }
+        }
+
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //SetMaxCount();
+        }
+
+        protected void ddlCluster_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //SetMaxCount();
+        }
+
+        private void SetMaxCount()
+        {
+            //string countryId = string.Empty;
+            //string clusterId = string.Empty;
+
+            if (Convert.ToInt32(ddlCountry.SelectedValue) > -1)
+                countryId = ddlCountry.SelectedValue;
+
+            if (Convert.ToInt32(ddlCluster.SelectedValue) > -1)
+                clusterId = ddlCluster.SelectedValue;
+
+            GetMaxCount("Key-" + countryId + clusterId);
+        }
+    }
+>>>>>>> d0744389ba73728c31d6842f0ffb9f0481af4c51
 }
