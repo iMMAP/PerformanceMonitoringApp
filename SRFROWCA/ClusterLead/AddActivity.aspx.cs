5637f39d4ce0975e938a30d58854d7022595f6ee
waseempk <waseem.asghar.pk@gmail.com>
2014-11-14 23:22:17 +0500

    Activity

---
+++ SRFROWCA/ClusterLead/AddActivity.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SRFROWCA.Common;
+using BusinessLogic;
+using System.Data;
+using System.Transactions;
+using SRFROWCA.Controls;
+using System.Net.Mail;
+using System.Text;
+using System.Web.Security;
+
+namespace SRFROWCA.ClusterLead
+{
+    public partial class AddActivity : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            UserInfo.UserProfileInfo();
+            PopulateObjective();
+            PopulateClusters();
+            PopulateCountries();           
+            ShowHideControls();
+           
+        }
+
+        private void ShowHideControls()
+        {
+            if (RC.IsClusterLead(this.User))
+            {
+                ddlCluster.Visible = false;
+                rfvCluster.Enabled = false;
+                dvcluster.Visible = false;
+                ddlCountry.Visible = false;
+                rfvCountry.Visible = false;
+                dvCountry.Visible = false;
+            }
+            else if (RC.IsCountryAdmin(this.User))
+            {
+                ddlCluster.Visible = true;
+                rfvCluster.Enabled = true;
+                dvcluster.Visible = true;
+                ddlCountry.Visible = false;
+                rfvCountry.Visible = false;
+                dvCountry.Visible = false;
+            }
+            else if (RC.IsAdmin(this.User) || RC.IsOCHAStaff(this.User))
+            {
+                ddlCluster.Visible = true;
+                rfvCluster.Enabled = true;
+                dvcluster.Visible = true;
+                ddlCountry.Visible = true;
+                rfvCountry.Visible = true;
+                dvCountry.Visible = true;
+            }
+        }
+
+        private void PopulateClusters()
+        {
+            int emgId = UserInfo.Emergency;
+            ddlCluster.DataValueField = "EmergencyClusterId";
+            ddlCluster.DataTextField = "ClusterName";
+
+            ddlCluster.DataSource = GetEmergencyClusters(emgId);
+            ddlCluster.DataBind();
+
+            ListItem item = new ListItem("Select Cluster", "0");
+            ddlCluster.Items.Insert(0, item);
+        }
+        private DataTable GetEmergencyClusters(int emergencyId)
+        {
+            return DBContext.GetData("GetEmergencyClusters", new object[] { emergencyId,RC.SelectedSiteLanguageId });
+        }
+
+        private void PopulateCountries()
+        {
+            ddlCountry.DataValueField = "LocationId";
+            ddlCountry.DataTextField = "LocationName";
+
+            ddlCountry.DataSource = DBContext.GetData("GetEmergencyCountries", new object[]{UserInfo.Emergency});
+            ddlCountry.DataBind();
+            ListItem item = new ListItem("Select Country", "0");
+            ddlCountry.Items.Insert(0, item);
+        }
+
+        private void PopulateObjective()
+        {
+            UI.FillObjectives(ddlObjective);
+            ddlObjective.DataSource = DBContext.GetData("GetEmergencyObjectives", new object[] {RC.SelectedSiteLanguageId,UserInfo.Emergency});
+            ddlObjective.DataTextField = "ShortObjectiveTitle";
+            ddlObjective.DataValueField = "EmergencyObjectiveId";
+            ddlObjective.DataBind();
+            ListItem item = new ListItem("Select Objective", "0");
+            ddlObjective.Items.Insert(0, item);
+        }
+
+      
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                SaveData();
+                scope.Complete();
+                Response.Redirect("ActivityListing.aspx");
+                
+            }
+        }
+
+        private void SaveData()
+        {
+            int ActivityId = SaveActivity();            
+        }
+
+        
+        private int SaveActivity()
+        {
+            int emergencyId = UserInfo.Emergency;
+            int clusterId = RC.IsClusterLead(this.User) ? UserInfo.Cluster : Convert.ToInt32(ddlCluster.SelectedValue);
+            int emergencyLocationId = RC.IsClusterLead(this.User) || RC.IsCountryAdmin(this.User) ? UserInfo.EmergencyCountry : Convert.ToInt32(ddlCountry.SelectedValue);
+            int emergencyClusterId = RC.IsClusterLead(this.User) ? UserInfo.EmergencyCluster : Convert.ToInt32(ddlCluster.SelectedValue); 
+
+            if (emergencyClusterId == 0)
+            {
+                if (Request.QueryString["cid"] != null)
+                {
+                    int.TryParse(Request.QueryString["cid"], out emergencyClusterId);
+                }
+            }
+
+            Guid userId = RC.GetCurrentUserId;
+
+            int objId = RC.GetSelectedIntVal(ddlObjective);            
+            string actEn = txtActivityEng.Text.Trim();
+            string actFr = txtActivityFr.Text.Trim();
+
+            return DBContext.Add("InsertActivityNew", new object[] { emergencyClusterId, objId,emergencyLocationId, 
+                                                                        actEn, actFr, userId, DBNull.Value });
+        }
+
+      
+       
+
+        protected void btnBackToSRPList_Click(object sender, EventArgs e)
+        {
+           Response.Redirect("~/ClusterLead/ActivityListing.aspx");
+        }
+    }
+}
