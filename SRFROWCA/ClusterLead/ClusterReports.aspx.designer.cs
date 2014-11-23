7c918879a7071258ab7d8699502885c3b37d6536
waseempk <waseem.asghar.pk@gmail.com>
2014-11-23 21:07:10 +0500

    ClusterOutPutReportPDF

--- SRFROWCA/ClusterLead/ClusterReports.aspx.designer.cs
+++ SRFROWCA/ClusterLead/ClusterReports.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace SRFROWCA.ClusterLead {
     public partial class ClusterReports {
         
         /// <summary>
+        /// btnExportPDF control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btnExportPDF;
+        
+        /// <summary>
         /// btnExportToExcel control.
         /// </summary>
         /// <remarks>
