7c6531673f8c2d3cd26092bfdb0f3e7ccd16dba0
KashifNadeem <kashif.nadeem@hotmail.com>
2014-12-11 23:43:03 +0500

    Regional Targets

---
+++ SRFROWCA/ClusterLead/SahelIndicatorTargets.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SahelIndicatorTargets.aspx.cs" Inherits="SRFROWCA.ClusterLead.SahelIndicatorTargets" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <script src="../assets/orsjs/jquery.wholenumber.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+
+        $(function () {
+            $(".numeric1").wholenumber();
+        });
+        </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="breadcrumbs" id="breadcrumbs">
+        <script type="text/javascript">
+            try { ace.settings.check('breadcrumbs', 'fixed') } catch (e) { }
+        </script>
+        <ul class="breadcrumb">
+            <li><i class="icon-home home-icon"></i><a href="#">Home</a> </li>
+            <li class="active">Sahel Indicator Target</li>
+        </ul>
+        <!-- .breadcrumb -->
+    </div>
+
+    <div class="page-content">
+        <div id="divMsg">
+        </div>
+        <table border="0" style="margin: auto; width: 100%">
+            <tr>
+                <td>
+                    <label>
+                        Clsuter:</label>
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlCluster" runat="server" Width="300px" AutoPostBack="true" OnSelectedIndexChanged="ddlCluster_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+
+                <td>
+                    <label>
+                        Country:</label>
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlCountry" runat="server" Width="300px">
+                    </asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="rfvCountry" runat="server" ErrorMessage="Required" Display="Dynamic"
+                        CssClass="error2" InitialValue="0" Text="Required" ControlToValidate="ddlCountry"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td style="text-align: right;">
+                    <asp:Button runat="server" ID="btnSave" Text="Save" class="width-20 btn btn-sm" OnClick="btnSave_Click" />
+                </td>
+            </tr>
+        </table>
+        <div class="table-responsive">
+            <div style="overflow-x: auto; width: 100%">
+                <asp:GridView ID="gvSahelIndTargets" runat="server" AutoGenerateColumns="false" ShowHeaderWhenEmpty="true"
+                    CssClass=" table-striped table-bordered table-hover" Width="100%"
+                    EmptyDataText="There are no output indicators available!" DataKeyNames="ClusterIndicatorId">
+                    <HeaderStyle BackColor="Control"></HeaderStyle>
+                    <Columns>
+                        <asp:TemplateField ItemStyle-Width="2%" HeaderText="#" meta:resourcekey="TemplateFieldResource1">
+                            <ItemTemplate>
+                                <%# Container.DataItemIndex + 1 %>
+                            </ItemTemplate>
+
+                            <ItemStyle Width="2%"></ItemStyle>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="ClusterIndicator" HeaderText="Sahel (Regional) Indicator" SortExpression="ClusterIndicator" ItemStyle-Width="70%" />
+                        <asp:BoundField DataField="RunningSum" HeaderText="Running Sum" SortExpression="RunningSum" />
+                        <asp:TemplateField HeaderText="Target" ItemStyle-Width="100px">
+                            <ItemTemplate>
+                                <asp:TextBox ID="txtTarget" runat="server" CssClass="numeric1" Text='<%# Eval("CountryClusterTarget") %>'></asp:TextBox>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ SRFROWCA/ClusterLead/SahelIndicatorTargets.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using SRFROWCA.Common;
+
+namespace SRFROWCA.ClusterLead
+{
+    public partial class SahelIndicatorTargets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+
+            LoadCombos();
+            DisableDropDowns();
+            LoadSahelIndicators();
+        }
+
+        private void LoadSahelIndicators()
+        {
+            gvSahelIndTargets.DataSource = GetSahelIndicators();
+            gvSahelIndTargets.DataBind();
+        }        
+
+        private void LoadCombos()
+        {
+            int emergencyId = RC.EmergencySahel2015;
+            UI.FillEmergencyLocations(ddlCountry, emergencyId);
+            UI.FillEmergnecyClusters(ddlCluster, emergencyId);
+            ddlCluster.Items.Insert(0, new ListItem("--- Select Cluster ---", "0"));
+            ddlCountry.Items.Insert(0, new ListItem("--- Select Country ---", "0"));
+            SetComboValues();
+        }
+
+        private void DisableDropDowns()
+        {
+            if (RC.IsClusterLead(this.User))
+            {
+                RC.EnableDisableControls(ddlCluster, false);
+                RC.EnableDisableControls(ddlCountry, false);
+            }
+
+            if (RC.IsCountryAdmin(this.User))
+            {
+                RC.EnableDisableControls(ddlCountry, false);
+            }
+        }
+
+        private void SetComboValues()
+        {
+            if (RC.IsClusterLead(this.User))
+            {
+                ddlCountry.SelectedValue = UserInfo.EmergencyCountry.ToString();
+                ddlCluster.SelectedValue = UserInfo.EmergencyCluster.ToString();
+            }
+
+            if (RC.IsCountryAdmin(this.User))
+            {
+                ddlCountry.SelectedValue = UserInfo.EmergencyCountry.ToString();
+            }
+        }
+
+        private DataTable GetSahelIndicators()
+        {
+            int val = RC.GetSelectedIntVal(ddlCluster);
+            int? emgClusterId = val > 0 ? val : (int?)null;
+            val = 0;
+
+            val = RC.GetSelectedIntVal(ddlCountry);
+            int? emgCountryId = val > 0 ? val : (int?)null;
+
+            return DBContext.GetData("GetSahelIndicatorsWithTargets", new object[] {RC.EmergencySahel2015,  emgClusterId, emgCountryId, RC.SelectedSiteLanguageId });            
+        }
+
+        protected void ddlCluster_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadSahelIndicators();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveTargets();
+            ShowMessage("Targets Saved Successfully");
+        }
+
+        private void SaveTargets()
+        {
+            foreach (GridViewRow row in gvSahelIndTargets.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    int clusterIndicatorId = 0;
+                    int.TryParse(gvSahelIndTargets.DataKeys[row.RowIndex].Values["ClusterIndicatorId"].ToString(), out clusterIndicatorId);
+                    if (clusterIndicatorId > 0)
+                    {
+                        int? target = GetIndicatorTarget(row);
+                        SaveIndicatorTarget(clusterIndicatorId, target);
+                    }
+                }
+            }
+        }
+
+        private void SaveIndicatorTarget(int clusterIndicatorId, int? target)
+        {
+            int emgLocationId = RC.GetSelectedIntVal(ddlCountry);
+            DBContext.Add("InsertSahelIndicatorCountryTarget", new object[] { clusterIndicatorId, emgLocationId, target, RC.GetCurrentUserId, DBNull.Value });
+        }
+
+        private int? GetIndicatorTarget(GridViewRow row)
+        {
+            int? target = null;
+            TextBox txtTarget = row.FindControl("txtTarget") as TextBox;
+            if (txtTarget != null)
+            {
+                if (!string.IsNullOrEmpty(txtTarget.Text.Trim()))
+                {
+                    target = Convert.ToInt32(txtTarget.Text.Trim());
+                }
+            }
+
+            return target;
+        }
+
+        private void ShowMessage(string message, RC.NotificationType notificationType = RC.NotificationType.Success, bool fadeOut = true, int animationTime = 500)
+        {
+            RC.ShowMessage(Page, typeof(Page), UniqueID, message, notificationType, fadeOut, animationTime);
+        }
+    }
+}
