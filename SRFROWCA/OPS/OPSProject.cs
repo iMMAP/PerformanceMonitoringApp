794fc6a0ad551569873a747c0beeecfd1a37a71f
KashifNadeem <kashif.nadeem@hotmail.com>
2015-01-13 22:57:24 +0500

    !

---
+++ SRFROWCA/OPS/OPSProject.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SRFROWCA.OPS
+{
+    public class OPSProject
+    {
+        public string ProjectId { get; set; }
+        public string ProjectCode { get; set; }
+        public string ProjectTitle { get; set; }
+        public string ProjectObjective { get; set; }
+        public string EmergencyLocationId { get; set; }
+        public string EmergencyClusterId { get; set; }
+        public string ProjectStatusId { get; set; }
+        public string BeneficiaryTotalNumber { get; set; }
+        public string BeneficiariesChildren { get; set; }
+        public string BeneficiariesWomen { get; set; }
+        public string BeneficiariesOthers { get; set; }
+        public string BeneficiariesDescription { get; set; }
+        public string BeneficiariesTotalDescription { get; set; }
+        public string ProjectStartDate { get; set; }
+        public string ProjectEndDate { get; set; }
+        public string ProjectImplementingpartner { get; set; }
+        public string ProjectContactName { get; set; }
+        public string ProjectContactEmail { get; set; }
+        public string ProjectContactPhone { get; set; }
+        public string RelatedURL { get; set; }
+        public string ProjectType { get; set; }
+        public string OPSLastUpdatedDate { get; set; }
+        public string OPSLastUpdatedBy { get; set; }
+        public string OPSProjectStatus { get; set; }
+        public string OPSClusterName { get; set; }
+        public string IsOPSProject { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedById { get; set; }
+        public string UpdatedDate { get; set; }
+        public string UpdatedById { get; set; }
+        public string ImportedById { get; set; }
+        public string ImportedDate { get; set; }
+        public string DonorName { get; set; }
+        public string FundingStatus { get; set; }
+        public string DonorName2 { get; set; }
+        public string Contribution2Amount { get; set; }
+        public string Contribution2CurrencyId { get; set; }
+        public string RequestedAmount { get; set; }
+        public string RequestedAmountCurrencyId { get; set; }
+        public string Contribution1Amount { get; set; }
+        public string Contribution1CurrencyId { get; set; }
+        public string ProjectStatus { get; set; }
+        public string Type { get; set; }
+        public string ProjectShortTitle { get; set; }
+        public string Pooled { get; set; }
+        public string ProjectYear { get; set; }
+        public string GenderMarker { get; set; }
+        public string AppealId { get; set; }
+        public string AppealName { get; set; }
+        public string SectorId { get; set; }
+        public string SectorName { get; set; }
+        public string ClusterId { get; set; }
+        public string ClusterName { get; set; }
+        public string PriorityId { get; set; }
+        public string PriorityName { get; set; }
+        public string CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string OtherFields { get; set; }
+        public string EGFLocations { get; set; }
+        public OPSDescription OPSDescriptions { get; set; }
+        public OPSProjectOrganizations Organizations { get; set; }
+    }
+
+    public class OPSDescription
+    {
+        public string Objectives { get; set; }
+        public string Needs { get; set; }
+        public string Activities { get; set; }
+        public string Outcomes { get; set; }
+    }
+
+    public class OPSProjectOrganizations
+    {
+        public List<OPSOrganization> Organization = new List<OPSOrganization>();
+    }
+
+    public class OPSOrganization
+    {
+        public string OrganizationId { get; set; }
+        public string OriginalRequirements { get; set; }
+        public string RevisedRequirements { get; set; }
+        public string OrgAbbrevation { get; set; }
+        public string OrganizationName { get; set; }
+        public string BudgetLineType { get; set; }
+        public string BudgetLineDescription { get; set; }
+        public string BudgetLineAmount { get; set; }
+    }
+
+    public class OPSBudgetLine
+    {
+        public string BudgetType { get; set; }
+        public string Description { get; set; }
+        public string Amount { get; set; }
+    }
+}
---
+++ SRFROWCA/OPS/SyncOPSProjects.aspx.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using BusinessLogic;
+using SRFROWCA.Common;
+
+namespace SRFROWCA.OPS
+{
+    public partial class SyncOPSProjects : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+
+            
+        }
+
+        protected void btnImportProjects_Click(object sender, EventArgs e)
+        {
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1075.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1076.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1077.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1078.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1079.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1080.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1081.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1082.xml");
+            InsertUpdateProjects("http://ops.unocha.org/api/v1/project/appeal/1083.xml");
+
+            Response.Write("All Synced!");
+        }
+
+        private void InsertUpdateProjects(string url)
+        {
+            try
+            {
+                Response.Write("<br/>");
+                Response.Write(url);
+                Response.Write("<br/>");
+                XDocument doc = XDocument.Load(url);
+                IEnumerable<OPSProject> projects = GetProjects(doc);
+                SyncProjects(projects, doc);
+            }
+            catch (Exception ex)
+            {
+                Response.Write(url);
+                Response.Write("<br/>");
+                Response.Write(ex.ToString());
+                Response.Write("<br/>");
+            }
+        }
+
+        private IEnumerable<OPSProject> GetProjects(XDocument doc)
+        {
+            //XDocument doc = XDocument.Load(url);
+            int so = 0;
+            var projects = doc.Root
+                .Elements("project")
+                .Select(x => new OPSProject
+                {
+                    ProjectId = (string)x.Attribute("id"),
+                    OPSProjectStatus = (string)x.Element("status"),
+                    Type = (string)x.Element("type"),
+                    ProjectCode = (string)x.Element("code"),
+                    ProjectTitle = (string)x.Element("title"),
+                    Pooled = (string)x.Element("pooled"),
+                    ProjectStartDate = (string)x.Element("startdate"),
+                    ProjectEndDate = (string)x.Element("enddate"),
+                    ProjectYear = (string)x.Element("year"),
+                    BeneficiariesChildren = (string)x.Element("beneficiaries_children"),
+                    BeneficiariesWomen = (string)x.Element("beneficiaries_women"),
+                    BeneficiaryTotalNumber = (string)x.Element("beneficiaries_total"),
+                    BeneficiariesTotalDescription = (string)x.Element("beneficiaries_total_description"),
+                    BeneficiariesOthers = (string)x.Element("beneficiaries_others"),
+                    BeneficiariesDescription = (string)x.Element("beneficiaries_others_description"),
+                    ProjectImplementingpartner = (string)x.Element("implementing_partners"),
+                    GenderMarker = (string)x.Element("gender_marker"),
+                    ProjectContactName = (string)x.Element("contact_name"),
+                    ProjectContactPhone = (string)x.Element("contact_phone"),
+                    ProjectContactEmail = (string)x.Element("contact_email"),
+                    AppealId = (string)x.Element("appeal").Attribute("id"),
+                    AppealName = (string)x.Element("appeal"),
+                    SectorId = (string)x.Element("sector").Attribute("id"),
+                    SectorName = (string)x.Element("sector"),
+                    ClusterId = (string)x.Element("cluster").Attribute("id"),
+                    ClusterName = (string)x.Element("cluster"),
+                    //PriorityId = (string)x.Element("priority").Attribute("id"),
+                    //PriorityName = (string)x.Element("priority"),
+                    CountryId = (string)x.Element("country").Attribute("id"),
+                    CountryName = (string)x.Element("country"),
+                    OtherFields = (string)x.Element("other_fields"),
+                    EGFLocations = (string)x.Element("egf_locations")
+                    ////Organizations = new OPSProjectOrganizations
+                    ////{
+                    ////    Organization = x.Descendants("organisations")
+                    ////                      .Select(o => new OPSOrganization
+                    ////                        {
+                    ////                            OrganizationId = (string)o.Element("organization").Attribute("id"),
+                    ////                            OrgAbbrevation = (string)o.Element("name").Attribute("abbreviation"),
+                    ////                            OrganizationName = (string)o.Element("name"),
+                    ////                            OriginalRequirements = (string)o.Element("original_requirement"),
+                    ////                            RevisedRequirements = (string)o.Element("revised_requirement")
+                    ////                        }).ToList()
+                    ////}
+                });
+
+            return projects;
+        }
+
+        private void SyncProjects(IEnumerable<OPSProject> projects, XDocument doc)
+        {
+            int k = projects.Count();
+            foreach (var project in projects)
+            {
+                if (project.SectorName != "SECTOR NOT YET SPECIFIED")
+                {
+                    object[] parameters = GetParameters(project);
+                    DBContext.Update("InsertUpdateOPSProject", parameters);
+                    Response.Write(project.ProjectId);
+                    Response.Write("<br/>");
+                    IEnumerable<OPSOrganization> organizations = GetOrganizations(doc, project.ProjectId);
+                    object[] orgParameter = GetOrganizationParameter(project.ProjectId, organizations.FirstOrDefault());
+                    DBContext.Update("InsertUpdateOPSProjectOrganization", orgParameter);
+                }
+
+            }
+        }
+
+        private IEnumerable<OPSOrganization> GetOrganizations(XDocument doc, string projectId)
+        {
+            var a = doc.Root.Elements("project")
+                .Descendants("organisations").Descendants("organisation");
+            var b = doc.Root.Elements("project")
+                .Where(p => ((string)p.Attribute("id")) == projectId)
+                .Descendants("organisations").Descendants("organisation");
+
+            var organizations = doc.Root.Elements("project")
+                .Where(p => ((string)p.Attribute("id")) == projectId)
+                .Descendants("organisations").Descendants("organisation")
+                .Select(o => new OPSOrganization
+                {
+                    OrganizationId = (string)o.Attribute("id"),
+                    OrgAbbrevation = (string)o.Element("name").Attribute("abbreviation"),
+                    OrganizationName = (string)o.Element("name"),
+                    OriginalRequirements = (string)o.Element("original_requirement"),
+                    RevisedRequirements = (string)o.Element("revised_requirement")
+                });
+
+            return organizations;
+        }
+
+        private object[] GetOrganizationParameter(string projId, OPSOrganization org)
+        {
+            int projectId = 0;
+            int.TryParse(projId, out projectId);
+            int organizationId = 0;
+            int.TryParse(org.OrganizationId, out organizationId);
+            int revisedReques = 0;
+            int.TryParse(org.RevisedRequirements, out revisedReques);
+            int originalRequest = 0;
+            int.TryParse(org.OriginalRequirements, out originalRequest);
+
+            return new object[] { projectId, organizationId, org.OrganizationName, org.OrgAbbrevation, originalRequest, revisedReques, RC.GetCurrentUserId, DBNull.Value };
+        }
+
+        private object[] GetParameters(OPSProject project)
+        {
+            int projectId = 0;
+            int.TryParse(project.ProjectId, out projectId);
+            int projectStatusId = 1;
+            
+            string clusterName = "";
+            if (project.ClusterName == "WATER AND SANITATION")
+            {
+                clusterName = "Water Sanitation & Hygiene";
+            }
+            else if (project.ClusterName == "MULTI-SECTOR FOR REFUGEES")
+            {
+                clusterName = "Multi Sector for Refugees";
+            }
+            else
+            {
+                clusterName = project.ClusterName;
+            }
+
+            return new object[] { RC.EmergencySahel2015, projectId, project.ProjectCode, project.ProjectTitle, project.ProjectObjective, 
+                                    project.CountryName, clusterName, projectStatusId, project.OPSProjectStatus, 
+                                    project.ProjectYear, project.ProjectContactName, project.ProjectContactPhone,
+                                    project.ProjectContactEmail, RC.GetCurrentUserId, DBNull.Value};
+        }
+    }
+}
