cb1d3f3f85717325099ecc7a697512d974050394
Kashif <kashif.nadeem@hotmail.com>
2015-09-23 21:28:34 +0000

    Migrate 2015 to 2016

---
+++ SRFROWCA/ClusterLead/IndicatorListingMigrate.aspx
@@ -0,0 +1,161 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="IndicatorListingMigrate.aspx.cs" Inherits="SRFROWCA.ClusterLead.IndicatorListingMigrate" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style>
+        .modalDialog {
+            width: 500px;
+            position: relative;
+            margin: 10% auto;
+            padding: 5px 20px 13px 20px;
+            border-radius: 2px;
+            background: #ffffff;
+        }
+    </style>
+    <script>
+        $(function () {
+            $('.tooltip2').tooltip();
+        });
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+   
+    <div class="page-content">
+        <table border="0" cellpadding="2" cellspacing="0" class="pstyle1" width="100%">
+            <tr>
+                <td class="signupheading2" colspan="3">
+                    <asp:UpdatePanel ID="updMessage" runat="server" UpdateMode="Conditional">
+                        <ContentTemplate>
+                            <div id="divMsg">
+                            </div>
+                        </ContentTemplate>
+                    </asp:UpdatePanel>
+                </td>
+            </tr>
+        </table>
+        <table width="100%">
+            <tr>
+                <td>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 ">
+                            <div class="widget-box">
+                                <div class="widget-header widget-header-small header-color-blue2">
+                                    <h6>
+                                        <asp:Button ID="btnMigrate" runat="server" Text="Migrate selected Indicators to 2016" CausesValidation="false"
+                                            CssClass="btn btn-danger pull-right" OnClick="btnMigrate_Click" />
+                                        <asp:Button ID="btnBackToFramework" runat="server" Text="Back to Framework" CausesValidation="false"
+                                            CssClass="btn btn-yellow pull-right" PostBackUrl="../ClusterLead/IndicatorListing.aspx" Style="margin-right: 5px;" />
+                                    </h6>
+                                </div>
+                                <div class="widget-body">
+                                    <div class="widget-main">
+                                        <div class="row">
+                                            <div class="col-xs-12">
+                                                <div class="row">
+                                                    <table border="0" style="width: 100%;">
+                                                        <tr>
+                                                            <td class="width-20">
+                                                                <label>
+                                                                    Cluster:</label>
+                                                            </td>
+                                                            <td class="width-30">
+                                                                <asp:DropDownList ID="ddlCluster" runat="server" CssClass="width-80">
+                                                                </asp:DropDownList>
+                                                            </td>
+                                                            <td>
+                                                                <label>Country:</label></td>
+                                                            <td>
+                                                                <asp:DropDownList runat="server" ID="ddlCountry" CssClass="width-80" meta:resourcekey="ddlCountryResource1">
+                                                                </asp:DropDownList></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="width-20">
+                                                                <label>
+                                                                    Objective:</label>
+                                                            </td>
+                                                            <td class="width-30">
+                                                                <asp:DropDownList ID="ddlObjective" runat="server" AppendDataBoundItems="true" CssClass="width-80">
+                                                                    <asp:ListItem Text="All" Value="-1" Selected="True"></asp:ListItem>
+
+                                                                </asp:DropDownList>
+                                                            </td>
+                                                            <td class="width-20">
+                                                                <label>
+                                                                    Activity:</label>
+                                                            </td>
+                                                            <td class="width-30">
+
+                                                                <asp:DropDownList ID="ddlActivity" runat="server" CssClass="width-80">
+                                                                </asp:DropDownList>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>&nbsp;</td>
+                                                            <td colspan="4" style="padding-top: 10px;">
+                                                                <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch2_Click" CssClass="btn btn-primary" CausesValidation="false" />
+                                                                <asp:Button ID="btnReset" runat="server" Text="Reset" Style="margin-left: 5px;" OnClick="btnReset_Click" CssClass="btn btn-primary" CausesValidation="False" meta:resourcekey="btnSearchResource1" />
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+
+        <div class="tablegrid">
+            <div style="overflow-x: auto; width: 100%">
+                <asp:GridView ID="gvActivity" runat="server" AutoGenerateColumns="false" AllowSorting="True" AllowPaging="true" Width="100%" 
+                    PagerSettings-Mode="NumericFirstLast" OnRowDataBound="gvActivity_RowDataBound" 
+                    PagerSettings-Position="Bottom" DataKeyNames="ActivityId,IndicatorId"
+                    CssClass="imagetable" OnSorting="gvActivity_Sorting" OnPageIndexChanging="gvActivity_PageIndexChanging"
+                    PageSize="70" ShowHeaderWhenEmpty="true" EmptyDataText="Your filter criteria does not match any indicator!">
+                    <Columns>
+                        <asp:TemplateField ItemStyle-Width="2%" HeaderText="#" meta:resourcekey="TemplateFieldResource1">
+                            <ItemTemplate>
+                                <%# Container.DataItemIndex + 1 %>
+                            </ItemTemplate>
+                            <ItemStyle Width="2%"></ItemStyle>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="Country" HeaderText="Country" SortExpression="Country" ItemStyle-Width="80px"></asp:BoundField>
+                        <asp:BoundField DataField="ClusterName" HeaderText="Cluster" SortExpression="ClusterName" ItemStyle-Width="150px" />
+                        <asp:BoundField DataField="ShortObjective" HeaderText="Objective" SortExpression="ShortObjective" ItemStyle-Width="90px" />
+                        <asp:BoundField DataField="Activity" HeaderText="Activity" SortExpression="Activity" />
+                        <asp:BoundField DataField="Indicator" HeaderText="Indicator" SortExpression="Indicator" />
+                        <asp:TemplateField HeaderText="Select" ItemStyle-HorizontalAlign="Center"
+                              ItemStyle-BackColor="Red">
+                            <ItemTemplate>
+                                <asp:CheckBox ID="cbIsSelected" runat="server" Checked='<%# Convert.ToBoolean(Eval("IsActive")) %>'/>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="Unit" HeaderText="Unit" SortExpression="Unit" />
+                        <asp:TemplateField ItemStyle-Width="4%" HeaderText="<span class='tooltip2' title='Each Indicator has assigned a calcuation method type.</br>Sum: Sum of all monthly achieved.</br>Agerage: Average of all monthly achieved.</br>Max: Max data reported in any month.</br>Latest: Latest data reported.'>Calculation Method</span>">
+                            <ItemTemplate>
+                                <asp:Label ID="lblCalcMethod" ToolTip="some text here" runat="server" Text=' <%# Eval("CalculationType")%>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField Visible="false" meta:resourcekey="TemplateFieldResource5">
+                            <ItemTemplate>
+                                <asp:Label ID="lblCountryID" runat="server" Text='<%# Eval("EmergencyLocationId") %>' meta:resourcekey="lblCountryIDResource1"></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField Visible="false" meta:resourcekey="TemplateFieldResource6">
+                            <ItemTemplate>
+                                <asp:Label ID="lblClusterID" runat="server" Text='<%# Eval("EmergencyClusterId") %>' meta:resourcekey="lblClusterIDResource1"></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                    <HeaderStyle BackColor="ButtonFace" />
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ SRFROWCA/ClusterLead/IndicatorListingMigrate.aspx.cs
@@ -0,0 +1,389 @@
+﻿using BusinessLogic;
+using SRFROWCA.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SRFROWCA.ClusterLead
+{
+    public partial class IndicatorListingMigrate : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                UserInfo.UserProfileInfo(RC.EmergencySahel2015);
+                LoadClustersFilter();
+                LoadCountry();
+                LoadObjectivesFilter();
+                PopulateActivities();
+                SetDropDownOnRole(true);
+                LoadIndicators();
+            }
+        }
+
+        private void SetDropDownOnRole(bool bindAll)
+        {
+            if (RC.IsClusterLead(this.User))
+            {
+                ddlCluster.SelectedValue = UserInfo.EmergencyCluster.ToString();
+                ddlCluster.Enabled = false;
+                ddlCluster.BackColor = Color.LightGray;
+                if (bindAll)
+                {
+                    ddlCountry.SelectedValue = UserInfo.EmergencyCountry.ToString();
+                    ddlCountry.Enabled = false;
+                    ddlCountry.BackColor = Color.LightGray;
+                }
+            }
+
+            if (RC.IsRegionalClusterLead(this.User))
+            {
+                ddlCluster.SelectedValue = UserInfo.EmergencyCluster.ToString();
+                ddlCluster.Enabled = false;
+                ddlCluster.BackColor = Color.LightGray;
+                if (bindAll)
+                {
+                    ddlCountry.SelectedValue = UserInfo.EmergencyCountry.ToString();
+                }
+            }
+
+            if (RC.IsCountryAdmin(this.User))
+            {
+                ddlCluster.SelectedValue = UserInfo.EmergencyCluster.ToString();
+                if (bindAll)
+                {
+                    ddlCountry.SelectedValue = UserInfo.EmergencyCountry.ToString();
+                    ddlCountry.Enabled = false;
+                    ddlCountry.BackColor = Color.LightGray;
+                }
+            }
+        }
+
+        // Add delete confirmation message with all delete buttons.
+        protected void gvActivity_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                CheckBox cbIsSelected = e.Row.FindControl("cbIsSelected") as CheckBox;
+                if (cbIsSelected != null)
+                    cbIsSelected.Enabled = !cbIsSelected.Checked;
+            }
+        }
+
+        // Execute row commands like Edit, Delete etc. on Grid.
+        protected void btnSearch2_Click(object sender, EventArgs e)
+        {
+            LoadIndicators();
+            PopulateActivities();
+        }
+
+        protected void btnMigrate_Click(object sender, EventArgs e)
+        {
+            foreach (GridViewRow row in gvActivity.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    int activityId = Convert.ToInt32(gvActivity.DataKeys[row.RowIndex].Values["ActivityId"].ToString());
+                    int indicatorId = Convert.ToInt32(gvActivity.DataKeys[row.RowIndex].Values["IndicatorId"].ToString());
+                    CheckBox cb = gvActivity.Rows[row.RowIndex].FindControl("cbIsSelected") as CheckBox;
+                    if (cb != null)
+                    {
+                        if (cb.Checked && cb.Enabled)
+                        {
+                            DBContext.Add("Insert2016Framework", new object[] { activityId, indicatorId, 12, RC.GetCurrentUserId, DBNull.Value });
+                        }
+                    }
+                }
+            }
+
+            Response.Redirect("~/ClusterLead/IndicatorListing.aspx");
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            if (RC.IsAdmin(this.User))
+            {
+                ddlCluster.SelectedValue = "0";
+                ddlActivity.SelectedValue = "0";
+                ddlCountry.SelectedValue = "0";
+            }
+            else
+            {
+                SetDropDownOnRole(true);
+            }
+
+            ddlObjective.SelectedValue = "0";
+            LoadIndicators();
+        }
+
+        internal override void BindGridData()
+        {
+            LoadClustersFilter();
+            LoadObjectivesFilter();
+            PopulateActivities();
+            SetDropDownOnRole(false);
+            LoadIndicators();
+        }
+
+        protected void gvActivity_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataTable dt = GetActivities();
+            if (dt != null)
+            {
+                //Sort the data.
+                dt.DefaultView.Sort = e.SortExpression + " " + GetSortDirection(e.SortExpression);
+                gvActivity.DataSource = dt;
+                gvActivity.DataBind();
+            }
+        }
+
+        private string GetSortDirection(string column)
+        {
+            // By default, set the sort direction to ascending.
+            string sortDirection = "ASC";
+
+            // Retrieve the last column that was sorted.
+            string sortExpression = ViewState["SortExpression"] as string;
+
+            if (sortExpression != null)
+            {
+                // Check if the same column is being sorted.
+                // Otherwise, the default value can be returned.
+                if (sortExpression == column)
+                {
+                    string lastDirection = ViewState["SortDirection"] as string;
+                    if ((lastDirection != null) && (lastDirection == "ASC"))
+                    {
+                        sortDirection = "DESC";
+                    }
+                }
+            }
+
+            // Save new values in ViewState.
+            ViewState["SortDirection"] = sortDirection;
+            ViewState["SortExpression"] = column;
+
+            return sortDirection;
+        }
+
+        private void LoadIndicators()
+        {
+            gvActivity.DataSource = GetActivities();
+            gvActivity.DataBind();
+        }
+
+        private void LoadCountry()
+        {
+            UI.FillEmergencyLocations(ddlCountry, RC.EmergencySahel2015);
+            ddlCountry.Items.Insert(0, new ListItem("--- Select Country ---", "0"));
+        }
+
+        private void PopulateActivities()
+        {
+            int? emergencyClusterId = ddlCluster.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlCluster.SelectedValue);
+            int? emergencyObjectiveId = ddlObjective.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlObjective.SelectedValue);
+            int? emergencyLocationId = ddlCountry.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlCountry.SelectedValue);
+
+            ddlActivity.DataSource = DBContext.GetData("GetActivitiesNew", new object[] { emergencyLocationId, emergencyClusterId, emergencyObjectiveId, RC.SelectedSiteLanguageId });
+            ddlActivity.DataTextField = "Activity";
+            ddlActivity.DataValueField = "ActivityId";
+            ddlActivity.DataBind();
+
+            ListItem item = new ListItem("Select Activity", "0");
+            ddlActivity.Items.Insert(0, item);
+        }
+
+        private void LoadClustersFilter()
+        {
+            UI.FillEmergnecyClusters(ddlCluster, RC.EmergencySahel2015);
+            ddlCluster.Items.Insert(0, new ListItem("--- Select Cluster ---", "0"));
+        }
+
+        private void LoadObjectivesFilter()
+        {
+            ddlObjective.Items.Clear();
+            ddlObjective.Items.Add(new ListItem("All", "0"));
+            ddlObjective.DataValueField = "EmergencyObjectiveId";
+            ddlObjective.DataTextField = "Objective";
+            ddlObjective.DataSource = GetObjectives();
+            ddlObjective.DataBind();
+        }
+
+        private DataTable GetClusters()
+        {
+            int emergencyId = RC.SelectedEmergencyId;
+            if (emergencyId == 0)
+            {
+                emergencyId = 1;
+            }
+
+            return DBContext.GetData("GetClusters", new object[] { (int)RC.SelectedSiteLanguageId, emergencyId });
+        }
+
+        private DataTable GetActivities()
+        {
+            int? emergencyClusterId = ddlCluster.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlCluster.SelectedValue);
+            int? emergencyObjectiveId = ddlObjective.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlObjective.SelectedValue);
+            int? activityId = ddlActivity.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlActivity.SelectedValue);
+            int? emergencyLocationId = ddlCountry.SelectedValue == "0" ? (int?)null : Convert.ToInt32(ddlCountry.SelectedValue);
+            return DBContext.GetData("GetAllIndicatorsToMigrate", new object[] { emergencyLocationId, emergencyClusterId, 
+                                                                                    emergencyObjectiveId, activityId, 
+                                                                                    (int)RC.SelectedSiteLanguageId });
+        }
+        private DataTable GetObjectives()
+        {
+            return DBContext.GetData("GetEmergencyObjectives", new object[] { (int)RC.SelectedSiteLanguageId, RC.EmergencySahel2015 });
+        }
+
+
+        private DataTable GetActivityTypes()
+        {
+
+            return DBContext.GetData("GetActivityTypes");
+
+        }
+
+        protected void Page_Error(object sender, EventArgs e)
+        {
+            // Get last error from the server
+            Exception exc = Server.GetLastError();
+            SRFROWCA.Common.ExceptionUtility.LogException(exc, "ActivityListing", this.User);
+        }
+
+        protected void gvActivity_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvActivity.PageIndex = e.NewPageIndex;
+            LoadIndicators();
+        }
+
+        private void ShowMessage(string message, RC.NotificationType notificationType = RC.NotificationType.Success, bool fadeOut = true, int animationTime = 500)
+        {
+            RC.ShowMessage(Page, typeof(Page), UniqueID, message, notificationType, fadeOut, animationTime);
+        }
+        
+        private void SetEmailItem(GridViewRow row, List<string> projects)
+        {
+            List<string> emailItems = new List<string>();
+            Label lblCountry = row.FindControl("lblCountryID") as Label;
+            if (lblCountry != null)
+            {
+                emailItems.Add(lblCountry.Text.Trim());
+            }
+
+            Label lblCluster = row.FindControl("lblClusterID") as Label;
+            if (lblCluster != null)
+            {
+                emailItems.Add(lblCluster.Text.Trim());
+            }
+            emailItems.Add(row.Cells[1].Text.ToString());
+            emailItems.Add(row.Cells[2].Text.ToString());
+            emailItems.Add(row.Cells[5].Text.ToString());
+            emailItems.Add(projects[0]);
+            emailItems.Add(projects[1]);
+            EmailItems = emailItems;
+        }
+
+        private void SendEmail(bool isAdded)
+        {
+            List<string> emailItems = EmailItems;
+            int emgCountryId = 0;
+            int.TryParse(emailItems[0], out emgCountryId);
+            int val = 0;
+            int.TryParse(emailItems[1], out val);
+            int? emgClusterId = val > 0 ? val : (int?)null;
+
+            string subject = "Activity Indicator has been disabled!";
+            if (isAdded)
+            {
+                subject = "Activity Indicator has been activated!";
+            }
+
+            string country = emailItems[2];
+            string cluster = emailItems[3];
+            string user = "";
+            try { user = User.Identity.Name; }
+            catch { }
+
+            string body = string.Format(@"<b>{0}</b><br/>
+                                         <b>Country:</b> {1}<br/>
+                                         <b>Cluster:</b> {2}<br/>
+                                         <b>Indicator:</b> {3}<br/>
+                                         <b>By:</b> {4}<br/>
+                                         <b>Projects Using This Indicator:</b> {5}"
+
+                                         , subject, country, cluster, emailItems[4], user, emailItems[5]);
+            DataTable dtUsers = DBContext.GetData("GetDataEntryUsersEmails", new object[] { emailItems[6], DBNull.Value, emgCountryId });
+            RC.SendEmail(emgCountryId, emgClusterId, subject, body, dtUsers);
+        }
+
+        private int IndicatorActiveStatus
+        {
+            get
+            {
+                int isActive = 0;
+                if (ViewState["IndicatorActiveStatusCB"] != null)
+                {
+                    int.TryParse(ViewState["IndicatorActiveStatusCB"].ToString(), out isActive);
+                }
+
+                return isActive;
+            }
+            set
+            {
+                ViewState["IndicatorActiveStatusCB"] = value;
+            }
+        }
+
+        private int ActivityActiveStatus
+        {
+            get
+            {
+                int isActive = 0;
+                if (ViewState["ActivityActiveStatusClsInds"] != null)
+                {
+                    int.TryParse(ViewState["ActivityActiveStatusClsInds"].ToString(), out isActive);
+                }
+
+                return isActive;
+            }
+            set
+            {
+                ViewState["ActivityActiveStatusClsInds"] = value;
+            }
+        }
+
+        private int IsIndicatorCheckBox
+        {
+            get
+            {
+                int isActive = 0;
+                if (ViewState["IsIndicatorCheckBox"] != null)
+                {
+                    int.TryParse(ViewState["IsIndicatorCheckBox"].ToString(), out isActive);
+                }
+
+                return isActive;
+            }
+            set
+            {
+                ViewState["IsIndicatorCheckBox"] = value;
+            }
+        }
+
+        private List<string> EmailItems
+        {
+            get
+            {
+                return ViewState["IndicatorActiveStatus"] as List<string>;
+            }
+            set
+            {
+                ViewState["IndicatorActiveStatus"] = value;
+            }
+        }
+    }
+}
