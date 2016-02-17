2cf4f9a5da2e52bbe0a09d688d3327280ac9d26b
Kashif <kashif.nadeem@hotmail.com>
2016-02-17 10:39:56 +0000

    !

---
+++ SRFROWCA/Common/Mail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
+using SRFROWCA.Admin;
+
+namespace SRFROWCA.Common
+{
+    public static class Mail
+    {
+        
+
+        internal static void SendMail(MailMessage mailMsg)
+        {
+            
+        }
+
+    }
+}
